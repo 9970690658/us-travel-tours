9609ff9867ac62d00863bcca8d15690dbae54aeb
Aamij Sutar <amijsutar456@gmail.com>
2026-09-13 22:38:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const fs = require("fs");
 // ---------------------------------------------------------
 
 require("dotenv").config({
-    path: path.join(__dirname, "..", ".env")
+    path: path.join(__dirname, ".env")
 });
 
 // ---------------------------------------------------------
